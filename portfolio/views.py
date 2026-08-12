5dc0fc176633d1807be4e8b7ef68d73e5b1432bc
saraswati <paudyalsaraswati2004@gmail.com>
2026-08-12 09:27:53 +0545

    Implement home view and add home.html template for portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Hello, I am building my portfolio!")
+    return render(request, 'portfolio/home.html')
