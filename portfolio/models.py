33fbe5b6e649c2c0880c55c7bc650ff3a95d7edc
saraswati <paudyalsaraswati2004@gmail.com>
2026-08-12 10:55:11 +0545

    Add Project model and update home view to display projects in home.html

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technologies = models.CharField(max_length=300)
+    github_link = models.URLField(blank=True)
+    live_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
